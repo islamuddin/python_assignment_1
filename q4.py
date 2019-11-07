a70d4cfa2c2b3cd0168acfb5fea21316beedf060
islamuddin <uddinislam46@gmail.com>
2019-11-07 10:10:44 +0500

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,5 @@
+# 4.
+print("=======[ Q.4 ]========")
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
