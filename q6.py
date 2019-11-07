3f7801033a74d3a86483454b52978063d7b99fa2
islamuddin <uddinislam46@gmail.com>
2019-11-07 10:11:29 +0500

    Create q6.py

---
+++ q6.py
@@ -0,0 +1,6 @@
+# 6.
+print("=======[ Q.6 ]========")
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+sum = float(num1) + float(num2)
+print('{0} + {1} = {2}'.format(num1, num2, sum))
